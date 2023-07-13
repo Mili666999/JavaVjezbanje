641d0a620101b5094823ecbeab722c767d51b083
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-07-13 13:32:48 +0200

    Riješeno sve do rekurzije za zbrajanje niza

--- src/java_vjezbanje/LjubavniKalkulator.java
+++ src/java_vjezbanje/LjubavniKalkulator.java
@@ -7,6 +7,20 @@ import javax.swing.JOptionPane;
 public class LjubavniKalkulator {
 	
 	
+	static int[] zbrojNiza(int [] brojevniNiz) {
+		//Provjera je li broj elemenata u brojevnom nizu paran
+		//Ako nije, dodajem 0 na kraj niza
+		int parniBrojevniNiz[] = new int[brojevniNiz.length+1];
+		if(brojevniNiz.length%2!=0) {
+			for(int i=0;i<brojevniNiz.length;i++) {
+				parniBrojevniNiz[i] = brojevniNiz[i];
+			}
+			brojevniNiz = parniBrojevniNiz.clone();
+		}
+		return brojevniNiz;
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		boolean dev = true;
@@ -33,19 +47,27 @@ public class LjubavniKalkulator {
 		//Sortiranje niza abecedno (zbog unosa dva imena različitim redoslijedom)
 		Arrays.sort(glavniNiz);
 		
+		//Kopiranje glavnog niza za usporedbu
+		char pomocniNiz[] = glavniNiz.clone();
+		
 		//Kreiranje i punjenje brojevnog niza
+		int brojevniNiz[] = new int[glavniNiz.length];
 		for(int i=0;i<glavniNiz.length;i++) {
-			
+			int x = 0;
+			for(int j=0;j<pomocniNiz.length;j++) {
+				if(glavniNiz[i]==pomocniNiz[j]) {
+					x++;
+					brojevniNiz[i]=x;
+				}
+			}
 		}
 		
 		
 		
 		
-		
 		System.out.println(Arrays.toString(glavniNiz));
-		
-		
-		
+		System.out.println(Arrays.toString(pomocniNiz));
+		System.out.println(Arrays.toString(brojevniNiz));	
 	}
 
 }
