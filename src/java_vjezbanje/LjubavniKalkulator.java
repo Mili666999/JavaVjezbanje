b102bd7945e863f71a8d7c86d7942d257c51d19a
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-07-13 13:52:23 +0200

    Nešt nevalja!

--- src/java_vjezbanje/LjubavniKalkulator.java
+++ src/java_vjezbanje/LjubavniKalkulator.java
@@ -10,14 +10,18 @@ public class LjubavniKalkulator {
 	static int[] zbrojNiza(int [] brojevniNiz) {
 		//Provjera je li broj elemenata u brojevnom nizu paran
 		//Ako nije, dodajem 0 na kraj niza
-		int parniBrojevniNiz[] = new int[brojevniNiz.length+1];
 		if(brojevniNiz.length%2!=0) {
+			int parniBrojevniNiz[] = new int [brojevniNiz.length+1];
 			for(int i=0;i<brojevniNiz.length;i++) {
 				parniBrojevniNiz[i] = brojevniNiz[i];
 			}
 			brojevniNiz = parniBrojevniNiz.clone();
 		}
-		return brojevniNiz;
+		int zadnjiNiz[] = new int [brojevniNiz.length/2];
+		if(zadnjiNiz.length>2) {
+			zbrojNiza(brojevniNiz);
+		}
+		return zadnjiNiz;
 	}
 	
 	
@@ -51,7 +55,7 @@ public class LjubavniKalkulator {
 		char pomocniNiz[] = glavniNiz.clone();
 		
 		//Kreiranje i punjenje brojevnog niza
-		int brojevniNiz[] = new int[glavniNiz.length];
+		int brojevniNiz[] = new int [glavniNiz.length];
 		for(int i=0;i<glavniNiz.length;i++) {
 			int x = 0;
 			for(int j=0;j<pomocniNiz.length;j++) {
@@ -61,13 +65,6 @@ public class LjubavniKalkulator {
 				}
 			}
 		}
-		
-		
-		
-		
-		System.out.println(Arrays.toString(glavniNiz));
-		System.out.println(Arrays.toString(pomocniNiz));
-		System.out.println(Arrays.toString(brojevniNiz));	
+		System.out.println(zbrojNiza(brojevniNiz));
 	}
-
 }
